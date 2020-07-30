950bad851c20704a58d238526c188d966ea40027
Cassidy James Blaede <c@ssidyjam.es>
2020-07-30 17:25:21 -0600

    Once-over of copy and styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,24 @@
 <template>
-  <div class="center">
-    <h1>elementary Build ISOs</h1>
+  <div>
+    <p>Note: This site hosts <strong>pre-release</strong> images and requires signing in with an authorized GitHub account to access. The latest stable version of elementary OS can always be downloaded at <a href="https://elementary.io">elementary.io</a>.</p>
 
-    <nuxt-link to="/downloads">
-      Download
-    </nuxt-link>
+    <div class="download">
+      <a class="button" href="https://elementary.io">
+        Visit elementary.io
+      </a>
+      <nuxt-link class="button suggested" to="/downloads">
+        See Daily Builds
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  a {
-    appearance: none;
-    background-color: var(--grape-700);
-    border-color: transparent;
-    border-radius: 3px;
-    border: none;
-    color: #fff;
-    display: inline-block;
-    font-family: inherit;
-    font-family: var(--ui-font);
-    font-size: 16px;
-    font-weight: 600;
-    margin: 24px;
-    min-width: 250px;
-    outline: none;
-    padding: 7px;
-    text-align: center;
-    text-decoration: none;
-    text-rendering: optimizeLegibility;
-    transition: opacity 200ms ease;
+  p {
+    max-width: var(--content-width);
   }
 
-  a:hover,
-  a:focus,
-  a:active {
-    background-color: var(--grape-500);
-    box-shadow:
-      0 2px 3px -1px rgba(73, 55, 147, 0.3),
-      0 5px 10px -2px rgba(156, 100, 218, 0.5);
-    opacity: 0.8;
+  .download {
+    text-align: center;
   }
 </style>
