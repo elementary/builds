a7925542b8b589d6c73fd6e19973682bbe98381e
Blake Kostner <git@btkostner.io>
2020-08-01 00:34:12 -0600

    wrap text

--- components/disclaimer.vue
+++ components/disclaimer.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p>These are <strong>pre-release</strong> daily builds of elementary OS. Things will be broken. Current major issues with these images may include, but are not limited to:</p>
+    <p>
+      These are <strong>pre-release</strong> daily builds of elementary OS.
+      Things will be broken. Current major issues with these images may include,
+      but are not limited to:
+    </p>
+
     <ul>
       <li>May require Internet to install</li>
       <li>Full-disk encryption may lead to a non-bootable install</li>
@@ -8,6 +13,11 @@
       <li>System stylesheet is unfinished</li>
       <li>General features, APIs are <strong>not finalized</strong></li>
     </ul>
-    <p>While these issues are expected to be resolved before a stable release of elementary OS 6, their existence now and the generally unfinished state mean <strong>do not use these images on a production system</strong>.</p>
+
+    <p>
+      While these issues are expected to be resolved before a stable release of
+      elementary OS 6, their existence now and the generally unfinished state
+      mean <strong>do not use these images on a production system</strong>.
+    </p>
   </div>
 </template>
