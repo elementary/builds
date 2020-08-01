78a4f994739c64a8b444a1725d2ce80be6d089ac
Cassidy James Blaede <c@ssidyjam.es>
2020-08-01 00:29:54 -0600

    Excessive disclaimer everywhere

---
+++ components/disclaimer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>These are <strong>pre-release</strong> daily builds of elementary OS. Things will be broken. Current major issues with these images may include, but are not limited to:</p>
+    <ul>
+      <li>May require Internet to install</li>
+      <li>Full-disk encryption may lead to a non-bootable install</li>
+      <li>Dark style support is unfinished</li>
+    </ul>
+    <p>While these issues are expected to be resolved before a stable release of elementary OS 6, their existence now and the generally unfinished state mean <strong>do not use these images on a production system</strong>.</p>
+  </div>
+</template>
