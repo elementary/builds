8a1ad7641389900378b2042087dd97f2cd524b33
Blake Kostner <git@btkostner.io>
2020-08-01 01:19:00 -0600

    check auth before redirect in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
 export default function ({ redirect, store }) {
   if (!store.getters['auth/loggedIn']) {
+    store.commit('auth/check')
+  }
+
+  if (!store.getters['auth/loggedIn']) {
     return redirect('/auth/login')
   }
 }
