9272b307956c6d7d82c6fdea656f6b8a5bee9c9e
Cassidy James Blaede <cassidy@elementary.io>
2020-08-01 00:34:09 -0600

    More line items

--- components/disclaimer.vue
+++ components/disclaimer.vue
@@ -5,6 +5,8 @@
       <li>May require Internet to install</li>
       <li>Full-disk encryption may lead to a non-bootable install</li>
       <li>Dark style support is unfinished</li>
+      <li>System stylesheet is unfinished</li>
+      <li>General features, APIs are <strong>not finalized</strong></li>
     </ul>
     <p>While these issues are expected to be resolved before a stable release of elementary OS 6, their existence now and the generally unfinished state mean <strong>do not use these images on a production system</strong>.</p>
   </div>
