522815b34531867668f08ef103bf5d9f72cb0cda
Daniel Foré <daniel@elementary.io>
2020-08-05 12:31:23 -0700

    Downloads: Rework copy

--- components/disclaimer.vue
+++ components/disclaimer.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <p>
-      These are <strong>pre-release</strong> daily builds of elementary OS.
-      Things will be broken. Please see the
-      <a href="https://github.com/orgs/elementary/projects/55" rel="noopener" target="_blank">6.0 Release GitHub Project</a>
-      for a full list of tasks and regressions. Current major issues with these
-      images may include, but are not limited to:
+      These are <strong>pre-release</strong> daily development builds of elementary OS.
+      Current major known issues include, but are not limited to:
     </p>
 
     <ul>
-      <li>May require Internet to install</li>
-      <li>Full-disk encryption may lead to a non-bootable install</li>
+      <li>Installation failures while installing offline or with full-disk encryption</li>
       <li>Curated apps are not available in AppCenter</li>
-      <li>Dark style support is unfinished</li>
-      <li>System stylesheet is unfinished</li>
-      <li>General features, APIs are <strong>not finalized</strong></li>
+      <li>Look & feel issues, including unfinished dark mode support</li>
+      <li>General features and APIs are <strong>not finalized</strong></li>
     </ul>
 
     <p>
-      While these issues are expected to be resolved before a stable release of
-      elementary OS 6, their existence now and the generally unfinished state
-      mean <strong>do not use these images on a production system</strong>.
+      Please see the
+      <a href="https://github.com/orgs/elementary/projects/55" rel="noopener" target="_blank">6.0 Release GitHub Project</a>
+      for a full list of outstanding tasks and regressions.
+    </p>
+
+    <p>
+      These issues, and more, are expected to be resolved before a stable release of elementary OS 6.
+      Until that time, <strong>do not use these images on a production system</strong>.
+    </p>
+
+    <p>
+      <strong>Please refrain from publishing reviews or other in-depth looks of early access builds.</strong>
+      We're excited that you're excited, but formally presenting unfinished products can damage our reputation. If you'd like to share informally, we'd appreciate a focus on features rather than quality or performance, which will change before release.
     </p>
   </div>
 </template>
