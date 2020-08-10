1428c8c18cbb42108cdd80befc4f2e89a77c1b83
Daniel Foré <daniel@elementary.io>
2020-08-09 17:00:33 -0700

    Sponsor: Make a note about private sponsors (#29)

--- pages/sponsor.vue
+++ pages/sponsor.vue
@@ -5,6 +5,9 @@
     <p>
       Help support elementary OS by <a href="https://github.com/sponsors/elementary">becoming a sponsor on GitHub</a>. Members of the <strong>$10/month tier</strong> and higher receive early access to pre-release daily builds as a perk of their sponsorship. Alternatively, visit <a href="https://elementary.io">elementary.io</a> for the latest stable version of elementary OS.
     </p>
+    <p>
+      If you're already a sponsor and are seeing this message, you may have to change your sponsorship from "private" to "public" in order for this website to verify your sponsorship.
+    </p>
 
     <div class="center">
       <a class="button" href="https://elementary.io">
