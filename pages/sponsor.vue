d236b67a9ecbd33a0ee16d5801027aee76e4ba3d
Cassidy James Blaede <c@ssidyjam.es>
2021-04-28 09:40:24 -0600

    Remove bit about public sponsorship

--- pages/sponsor.vue
+++ pages/sponsor.vue
@@ -7,9 +7,6 @@
     <p>
       Help support elementary OS by <a href="https://github.com/sponsors/elementary">becoming a sponsor on GitHub</a>. Members of the <strong>$10/month tier</strong> and higher receive Early Access to daily builds and builds for experimental platforms like Raspberry Pi 4 and Pinebook Pro as a perk of their sponsorship. Alternatively, visit <a href="https://elementary.io">elementary.io</a> for the latest stable version of elementary OS.
     </p>
-    <p>
-      If you're already a sponsor and are seeing this message, you may have to change your sponsorship from "private" to "public" in order for this website to verify your sponsorship.
-    </p>
 
     <div class="center">
       <a class="button" href="https://elementary.io">
